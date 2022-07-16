d823352d621f3a74494b545794031e78d3e9c639
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2022-07-16 12:30:01 +0300

    custom exception
    Signed-off-by: Ömer Faruk Karadeniz <krdnzomer@gmail.com>

---
+++ src/main/java/com/farukkaradeniz/qrgenerator/data/exception/QrException.java
@@ -0,0 +1,7 @@
+package com.farukkaradeniz.qrgenerator.data.exception;
+
+public class QrException extends RuntimeException {
+    public QrException(String message) {
+        super(message);
+    }
+}
