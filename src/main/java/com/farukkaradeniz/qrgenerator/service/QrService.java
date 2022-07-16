ae9aeb451676dce8937009f933fda10ffbd8fbf4
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2022-07-16 12:30:38 +0300

    error handling for outputstream try-with-resources
    Signed-off-by: Ömer Faruk Karadeniz <krdnzomer@gmail.com>

--- src/main/java/com/farukkaradeniz/qrgenerator/service/QrService.java
+++ src/main/java/com/farukkaradeniz/qrgenerator/service/QrService.java
@@ -3,39 +3,47 @@ package com.farukkaradeniz.qrgenerator.service;
 import com.farukkaradeniz.qrgenerator.data.constant.QrConstants;
 import com.farukkaradeniz.qrgenerator.data.dto.CreateQrRequestDTO;
 import com.farukkaradeniz.qrgenerator.data.dto.CreateQrResponseDTO;
+import com.farukkaradeniz.qrgenerator.data.exception.QrException;
 import com.farukkaradeniz.qrgenerator.util.Utils;
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.qrcode.QRCodeWriter;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class QrService {
 
-    @SneakyThrows
+
     public CreateQrResponseDTO createQrCode(CreateQrRequestDTO request) {
         log.info("Create QR Request {}", request);
 
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        var bitMatrix = qrCodeWriter.encode(request.getText(), BarcodeFormat.QR_CODE, request.getQrSize(), request.getQrSize());
-        var outputStream = new ByteArrayOutputStream();
 
-        // Set QR code colors config
-        var config = new MatrixToImageConfig(Utils.parseColorHexInteger(request.getColor()),
-                Utils.parseColorHexInteger(request.getBackgroundColor()));
+        try (
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
+        ) {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            var bitMatrix = qrCodeWriter.encode(request.getText(), BarcodeFormat.QR_CODE, request.getQrSize(), request.getQrSize());
 
-        MatrixToImageWriter.writeToStream(bitMatrix, QrConstants.QR_IMAGE_FORMAT, outputStream, config);
-        byte[] bytes = outputStream.toByteArray();
+            // Set QR code colors config
+            var config = new MatrixToImageConfig(Utils.parseColorHexInteger(request.getColor()),
+                    Utils.parseColorHexInteger(request.getBackgroundColor()));
 
-        return CreateQrResponseDTO.builder()
-                .body(bytes)
-                .build();
+            MatrixToImageWriter.writeToStream(bitMatrix, QrConstants.QR_IMAGE_FORMAT, outputStream, config);
+            byte[] bytes = outputStream.toByteArray();
+
+            return CreateQrResponseDTO.builder()
+                    .body(bytes)
+                    .build();
+        } catch (WriterException | IOException e) {
+            throw new QrException("Error occurred while generating QR Code");
+        }
     }
 
 
