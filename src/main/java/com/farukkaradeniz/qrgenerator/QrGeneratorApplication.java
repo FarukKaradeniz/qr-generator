682c1bc99ed987527bba80e3955037037557bb48
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2023-04-02 15:02:31 +0300

    add openapi
    Signed-off-by: Ömer Faruk Karadeniz <krdnzomer@gmail.com>

--- src/main/java/com/farukkaradeniz/qrgenerator/QrGeneratorApplication.java
+++ src/main/java/com/farukkaradeniz/qrgenerator/QrGeneratorApplication.java
@@ -1,11 +1,18 @@
 package com.farukkaradeniz.qrgenerator;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "QR Generator",
+        version = "1.0",
+        description = "This application generates a QR code with given parameters")
+)
 public class QrGeneratorApplication {
 
     public static void main(String[] args) {
