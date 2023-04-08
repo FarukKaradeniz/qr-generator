a84bf82089857babc67e45206b0f6476a3b54a14
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2023-04-08 15:16:48 +0300

    micrometer
    Signed-off-by: Ömer Faruk Karadeniz <krdnzomer@gmail.com>

--- src/main/java/com/farukkaradeniz/qrgenerator/controller/QrController.java
+++ src/main/java/com/farukkaradeniz/qrgenerator/controller/QrController.java
@@ -3,6 +3,7 @@ package com.farukkaradeniz.qrgenerator.controller;
 import com.farukkaradeniz.qrgenerator.controller.model.request.CreateQrRequest;
 import com.farukkaradeniz.qrgenerator.mapper.QrMapper;
 import com.farukkaradeniz.qrgenerator.service.QrService;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -24,6 +25,7 @@ public class QrController {
     private final QrService qrService;
 
     @PostMapping(produces = MediaType.IMAGE_PNG_VALUE)
+    @Timed(value = "request_timer.createQr", description = "Time taken to create qr code")
     public ResponseEntity<byte[]> createQr(@Valid @RequestBody CreateQrRequest request) {
         var responseDTO = qrService.createQrCode(QrMapper.toCreateQrRequestDTO(request));
         return new ResponseEntity<>(QrMapper.toCreateQrResponse(responseDTO).getBody(), HttpStatus.CREATED);
